a5db4e88cc51362ae3a40f65ab8ed88b08175179
ANGELOUCO <121191378+ANGELOUCO@users.noreply.github.com>
2023-04-10 15:16:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // Handles loading the events for <model-viewer>'s slotted progress bar
 const onProgress = (event) => {
+  const audioElement = document.createElement('audio');
+audioElement.setAttribute('src', 'quack-quack.ogg');
+audioElement.setAttribute('preload', 'auto');
+
+document.body.appendChild(audioElement);
   const progressBar = event.target.querySelector('.progress-bar');
   const updatingBar = event.target.querySelector('.update-bar');
   updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+ 
   if (event.detail.totalProgress === 1) {
     progressBar.classList.add('hide');
     event.target.removeEventListener('progress', onProgress);
+     audioElement.play();
   } else {
     progressBar.classList.remove('hide');
   }
